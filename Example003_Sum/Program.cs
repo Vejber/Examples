6cab87a20bf264a21820f51b9915fefa656734bc
Vejber <vejber2013@gmail.com>
2022-08-04 16:01:12 +0400

    Added random numbers

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numberA = 3;
-int numberB = 5;
+int numberA = new Random().Next(1, 10); //1,2,3..9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);

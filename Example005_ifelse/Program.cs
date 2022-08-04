494245326cacadc809bc8573662ab6cd1cd1c1bf
Vejber <vejber2013@gmail.com>
2022-08-04 16:08:43 +0400

    Added ifelse

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter username");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+  Console.WriteLine("Hooray, it's Masha");
+}
+else
+{
+  Console.Write ("Hello, ");
+  Console.WriteLine (username);
+}
